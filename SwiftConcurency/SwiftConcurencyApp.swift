885884c308658e2ddba4e71387b7b97b42c9f78d
aembadawy <aem.badawy@gmail.com>
2025-05-26 10:32:25 +0300

    Initial Commit

---
+++ SwiftConcurency/SwiftConcurencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurencyApp.swift
+//  SwiftConcurency
+//
+//  Created by Aya on 26/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConcurencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftConcurencyTests/SwiftConcurencyTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurencyTests.swift
+//  SwiftConcurencyTests
+//
+//  Created by Aya on 26/05/2025.
+//
+
+import Testing
+@testable import SwiftConcurency
+
+struct SwiftConcurencyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
