f353f51e320d8e27673e24b7b98a6a1f22096713
aembadawy <aem.badawy@gmail.com>
2025-06-03 22:22:40 +0300

    Add AsyncStream Module

---
+++ SwiftConcurency/Modules/AsyncStream/AsyncStream.swift
@@ -0,0 +1,18 @@
+//
+//  AsyncStream.swift
+//  SwiftConcurency
+//
+//  Created by Aya on 03/06/2025.
+//
+
+import SwiftUI
+
+struct AsyncStream: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AsyncStream()
+}
