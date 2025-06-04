e5117c0da39b1967407e2a16926544f6bb916ae6
aembadawy <aem.badawy@gmail.com>
2025-06-04 18:46:46 +0300

    Add AsyncStream

--- SwiftConcurency/Modules/AsyncStream/AsyncStream.swift
+++ SwiftConcurency/Modules/AsyncStream/AsyncStream.swift
@@ -7,12 +7,66 @@
 
 import SwiftUI
 
-struct AsyncStream: View {
+class AsyncStreamDataProvider {
+    let prices = [105264, 105891, 106809, 109876, 110876]
+    
+    func getPrices(completion: @escaping(Int) -> Void ) {
+        for i in 0..<prices.count {
+            DispatchQueue.main.asyncAfter(deadline: .now() + Double(i)) {
+                completion(self.prices[i])
+            }
+        }
+    }
+    
+    func getAsyncStream() -> AsyncStream<Int> {
+        return AsyncStream { continuation in
+            self.getPrices { price in
+                continuation.yield(price)
+            }
+        }
+    }
+}
+
+class AsyncStreamViewModel: ObservableObject {
+    @Published var currentPrice: Int = 0
+    private let dataProvider = AsyncStreamDataProvider()
+    
+    init() {
+        getDataWithCompeletionHandler()
+    }
+    
+    func getDataWithCompeletionHandler() {
+        dataProvider.getPrices { [weak self] price in
+            self?.currentPrice = price
+        }
+    }
+    
+    func getDataWithStream() async {
+        for await price in dataProvider.getAsyncStream() {
+            self.currentPrice = price
+        }
+    }
+}
+
+struct AsyncStreamUIView: View {
+    @StateObject var viewModel = AsyncStreamViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        HStack {
+            Text("BTC")
+                .fontWeight(.bold)
+            Spacer()
+            
+            Text("$\(viewModel.currentPrice)")
+        }
+        .padding()
+        .task {
+            await viewModel.getDataWithStream()
+        }
     }
 }
 
 #Preview {
-    AsyncStream()
+    AsyncStreamUIView()
 }
